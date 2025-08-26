8fb3d10502197ad2259fe42285f1b6b47e2c3479
Schwimelmo69 <timo.schulze@hochschule-stralsund.de>
2025-08-26 15:14:23 +0200

    TUC und DTD
    aber es schlupft noch falsch

--- Fzg_par.m
+++ Fzg_par.m
@@ -13,17 +13,22 @@ v_x = 10;                   % [m/s] Längsgeschwindigkeit
 % Massen
 %--------------------------------------------------------------------------
 m_fzg = 241.5 + 82;         % [kg] Gesamtfahrzeugmasse
+m_Front = m_fzg * 0.4;      % [kg] Masse Front
+m_Rear = m_fzg - m_Front;   % [kg] Masse Rear
 J_fzg_z = 300;              % [kg*m^2] Massenträgheitsmoment um Z-Achse
 
 % Schwerpunkt und geometrische Abmaße
 %--------------------------------------------------------------------------
-l_fzg = 1300/1000;         % [mm] Radstand Fahrzeug
+l_fzg = 1300/1000;          % [mm] Radstand Fahrzeug
 l_V_fzg = 1023/1000;        % [mm] Schwerpunktvorlage
 l_H_fzg = 507/1000;         % [mm] Schwerpunktrücklage
+h_COG = 300/1000;           % [mm] Schwerpunkthöhe
+t_V_fzg = 1250/1000;        % [mm] Spurweite Front
+t_H_fzg = 1200/1000;        % [mm] Spurweite Rear
 
 % Lenkung
 %--------------------------------------------------------------------------
-delta_L = -5;                % [°] Einlenkwinkel Lenkrad
+delta_L = -5;               % [°] Einlenkwinkel Lenkrad
 i_L = 4.93;                 % [-] Lenkübersetzung
 delta_V = delta_L/i_L;      % [°] Spurwinkel Vorderrad
 delta_S = delta_V;
@@ -33,9 +38,17 @@ delta_H = 0;                % [°] Spurwinkel Hinterrad
 %--------------------------------------------------------------------------
 c_alpha_V = 9300;           % [N/rad] Vorderreifenseitenachssteifigkeit
 c_alpha_H = 13400;          % [N/rad] Hinterreifenseitenachssteifigkeit
+Dyn_Reifenradius = 0.23;    % [m] dynamischer Reifenradius
+Sturzwinkel = 3;            % [°] Sturzwinkel
+
+% Aerodynamik
+%--------------------------------------------------------------------------
+c_DA = 3.9;                 % [m^2] Downforcekoeffizient*Anströmfläche
+aero_balance_rear = 55/100; % [%] Aero Balance Rear
 
 
 %Einspurmodell Zustandsraum
+%--------------------------------------------------------------------------
 ss_A = [-(c_alpha_V*l_V_fzg^2+c_alpha_H*l_H_fzg^2)/(J_fzg_z*v_x) (c_alpha_H*l_H_fzg-c_alpha_V*l_V_fzg)/J_fzg_z 
         (c_alpha_H*l_H_fzg-c_alpha_V*l_V_fzg-m_fzg*v_x^2)/(m_fzg*v_x^2) -(c_alpha_V+c_alpha_H)/(m_fzg*v_x)];
 ss_B = [(c_alpha_V*l_V_fzg)/J_fzg_z -(c_alpha_H*l_H_fzg)/J_fzg_z
